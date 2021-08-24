e846335538b1857e150297c44945129aea06e65f
Pradip Sabhadiya <pradip@oneit.com.au>
2021-08-24 21:51:22 +0530

    3. Looking back at implementation of the different ItemRepo methods; Is Set best collection type to be used in terms of performance? Or we have a better alternative collection type?

---
+++ src/troubleshooting/question2/Test3.java
@@ -0,0 +1,14 @@
+package troubleshooting.question2;
+
+/**
+ * 3. Looking back at implementation of the different ItemRepo methods; Is Set best collection type to be used in terms of performance? Or we have a better alternative collection type?
+ * @author Pradip J. Sabhadiya<pradip@oneit.com.au>
+ */
+public class Test3
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Question : Looking back at implementation of the different ItemRepo methods; Is Set best collection type to be used in terms of performance? Or we have a better alternative collection type?");
+        System.out.println("Answer : It depends on the requirement. Let's say in terms of memory, Set is better. In terms of searching algorithm, Set is better. In terms of insertion algorithm, List is better.");
+    }
+}
