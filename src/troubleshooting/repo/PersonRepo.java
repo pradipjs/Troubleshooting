7c8dc0da1e3848d1b83d7c004377db29c5c56333
Pradip Sabhadiya <pradip@oneit.com.au>
2021-08-24 21:27:17 +0530

    6. Add a new searchByName() method that takes a second kind parameter (of type to be determined) returning Student type in case kind was student and returning Staff type in case kind was staff.

--- src/troubleshooting/repo/PersonRepo.java
+++ src/troubleshooting/repo/PersonRepo.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import troubleshooting.models.Person;
+import troubleshooting.utils.Utils;
 
 /**
  *
@@ -12,14 +13,39 @@ import troubleshooting.models.Person;
  */
 public class PersonRepo
 {
-    private Map<Class<? extends Person>, List<? extends Person>>  data    =   new HashMap<>();
+    private Map<Class, List<? extends Person>>  data    =   new HashMap<>();
 
     public <T extends Person> void addPerson(T person)
     {
-        List<T> personsList =   (List<T>) data.getOrDefault(person.getClass(), new ArrayList<T>());
-        
+        List<T> personsList =   getPersons((Class<T>) person.getClass());
+
         personsList.add(person);
-        
+
         data.put(person.getClass(), personsList);
+    }
+    
+    protected <T extends Person> List<T> getPersons(Class<T> kind)
+    {
+        if (!data.containsKey(kind))
+        {
+            data.put(kind, new ArrayList<T>());
+        }
+        
+        return (List<T>) data.get(kind);
+    }
+
+    public <T extends Person> T searchByName(String name, Class<T> kind)
+    {
+        List<T> personsList =   getPersons(kind);
+        
+        for (T t : personsList)
+        {
+            if (Utils.equalsIgnoreCase(name, t.getName()))
+            {
+                return t;
+            }
+        }
+        
+        return null;
     }
 }
