ef1497d56e21611934f88492c27d33632734ebfa
Pradip Sabhadiya <pradip@oneit.com.au>
2021-08-25 17:20:22 +0530

    4. Suppose that adding item to a repo is an expensive operation (see modified implementation of putItem() below) and we want to move it to a background thread. Modify the implementation putItem() to: 1) execute actualPutItem() on a background thread and 2) notify caller about the completion of the operation through a callback

--- src/troubleshooting/question2/Test2.java
+++ src/troubleshooting/question2/Test2.java
@@ -9,16 +9,23 @@ import troubleshooting.question2.repo.ItemRepo;
  */
 public class Test2
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException, Exception
     {
         ItemRepo repo = new ItemRepo();
-        repo.putItem(new Item(1001, "Joe"));
+        repo.putItem(new Item(1001, "Joe"), Test2::callback);
         // Name 'Joe' was wrong name so we want to fix it as 'Jonathan'
-        repo.putItem(new Item(1001, "Jonathan"));
+        repo.putItem(new Item(1001, "Jonathan"), Test2::callback);
         
         System.out.println("Question : Did statement on line 4 fix the name inserted item at line 2 or we have now a duplicate item? If we have duplicate item then how can we fix this problem? we need to have any two items with same id be treated as same one.");
         System.out.println("Anwser : We will have a duplicate item.");
         System.out.println("Solution 1 : You can call getItemById() method to get the existing first and If you find it, update in it.");
         System.out.println("Solution 2 : Manage using the putItem() function. First it will internally call the getItemById() method. If it finds any record, it will update in same, otherwise It will add a new one.");
+    }
+    
+    public static Void callback()
+    {
+        System.out.println("Finished job.");
+        
+        return null;
     }
 }
--- src/troubleshooting/question2/repo/ItemRepo.java
+++ src/troubleshooting/question2/repo/ItemRepo.java
@@ -2,6 +2,9 @@ package troubleshooting.question2.repo;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import troubleshooting.question2.models.Item;
 
 /**
@@ -12,26 +15,47 @@ public class ItemRepo
 {
     private Set<Item> items = new HashSet<>();
 
-    public void putItem(Item item)
+    public void putItem(Item item, Callable<Void> func) throws InterruptedException, Exception
     {
-        Item    itemToBeAdded   =   getItemById(item.getId());
+        Runnable r = () ->
+        {
+            try
+            {
+                actualPutItem(item);
+            }
+            catch (InterruptedException ex)
+            {
+                Logger.getLogger(ItemRepo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        };
         
+        Thread t    =   new Thread(r);
+        t.start();
+        t.join();
+        
+        func.call();
+    }
+
+    private synchronized void actualPutItem(Item item) throws InterruptedException
+    {
+        // Simulate an expensive operation
+        Thread.sleep(2000);
+
+        Item itemToBeAdded = getItemById(item.getId());
+
         if (itemToBeAdded == null)
         {
             items.add(item);
-        }
-        else
+        } else
         {
             itemToBeAdded.setName(item.getName());
         }
-        
-        System.out.println(items);
     }
 
     public void removeItemById(int itemId)
     {
-        Item    item    =   getItemById(itemId);
-        
+        Item item = getItemById(itemId);
+
         if (item != null)
         {
             items.remove(item);
@@ -47,7 +71,7 @@ public class ItemRepo
                 return item;
             }
         }
-        
+
         return null;
     }
 }
