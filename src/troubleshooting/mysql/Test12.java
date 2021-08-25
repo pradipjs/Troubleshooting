93d9a6fde547c27b2d607f4256a602054aefaf6e
Pradip Sabhadiya <pradip@oneit.com.au>
2021-08-25 18:35:46 +0530

    2. Recreate the query that returned the 10 rows of data supplied. Speculate on tables that would be needed that are not shown here

---
+++ src/troubleshooting/mysql/Test12.java
@@ -0,0 +1,15 @@
+package troubleshooting.mysql;
+
+/**
+ * Recreate the query that returned the 10 rows of data supplied. Speculate on tables that would be needed that are not shown here
+ * 
+ * @author Pradip J. Sabhadiya<pradip@oneit.com.au>
+ */
+public class Test12
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Query :: I have used full join for getting 10 records");
+        System.out.println("SELECT user.user_firstname, user.user_lastname, skill.skill_name FROM user, user_skill, skill WHERE user.id = user_skill.user_id AND skill.id = user_skill.skill_id LIMIT 10");
+    }
+}
